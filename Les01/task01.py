5a0057f83c516854a42decacfe083c4ff83c5a0f
xAlex099x <Alex09920000@gmail.com>
2023-03-01 13:52:48 +0900

    добавлены решения задач

--- Les01/task01.py
+++
@@ -1,7 +0,0 @@
-# Найдите сумму цифр трехзначного числа.
-number = int(input('Введите число:' ))
-sum = 0
-while number > 0:
-    sum = sum + number % 10
-    number = number // 10
-print('Сумма цифр в числе равна:',sum)
